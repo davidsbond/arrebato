9edf03e9a098621c396b00c7f09a2c431f6537b8
David Bond <davidsbond93@gmail.com>
2022-05-02 00:49:05 +0100

    Add persistence logic for topic assignment
    This commit adds the raft log handling logic for topic assignment. When the command
    comes in via the raft log, the node data in the state is updated to include the name
    of the newly assigned topic. All other records are also checked and the topic
    is removed from those records.
    
    Signed-off-by: David Bond <davidsbond93@gmail.com>

--- internal/node/store_test.go
+++ internal/node/store_test.go
@@ -3,6 +3,7 @@ package node_test
 import (
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
 	"github.com/davidsbond/arrebato/internal/node"
@@ -42,7 +43,58 @@ func TestBoltStore_Delete(t *testing.T) {
 		require.NoError(t, store.Delete(ctx, "test"))
 	})
 
-	t.Run("It should not return an error for a non-existant node", func(t *testing.T) {
+	t.Run("It should not return an error for a non-existent node", func(t *testing.T) {
 		require.NoError(t, store.Delete(ctx, "test"))
+	})
+}
+
+func TestBoltStore_AssignTopic(t *testing.T) {
+	t.Parallel()
+
+	ctx := testutil.Context(t)
+	db := testutil.BoltDB(t)
+	store := node.NewBoltStore(db)
+
+	// Create some nodes to assign topics to
+	require.NoError(t, store.Create(ctx, &nodepb.Node{
+		Name: "node-0",
+	}))
+	require.NoError(t, store.Create(ctx, &nodepb.Node{
+		Name: "node-1",
+	}))
+
+	const topicName = "test-topic"
+	t.Run("It should assign the topic to a node", func(t *testing.T) {
+		const nodeName = "node-0"
+
+		require.NoError(t, store.AssignTopic(ctx, nodeName, topicName))
+
+		nodes, err := store.List(ctx)
+		require.NoError(t, err)
+		for _, n := range nodes {
+			if n.GetName() == nodeName {
+				assert.Contains(t, n.GetTopics(), topicName)
+				continue
+			}
+
+			assert.NotContains(t, n.GetTopics(), topicName)
+		}
+	})
+
+	t.Run("It should reassign the topic to another node", func(t *testing.T) {
+		const nodeName = "node-1"
+
+		require.NoError(t, store.AssignTopic(ctx, nodeName, topicName))
+
+		nodes, err := store.List(ctx)
+		require.NoError(t, err)
+		for _, n := range nodes {
+			if n.GetName() == nodeName {
+				assert.Contains(t, n.GetTopics(), topicName)
+				continue
+			}
+
+			assert.NotContains(t, n.GetTopics(), topicName)
+		}
 	})
 }
